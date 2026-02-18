0ba728fe53251f4d96c1bc34aa4cee0927090e39
LeoSouzaSenac <ledsouza@senacrs.com.br>
2026-02-18 16:08:11 -0300

    corrigida funcao somar e adicionados comentarios

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+/*
+    1) Criar um novo repositório no github 
+    (marque a opção README)
+    2) Clonar este repositório na pasta documentos
+    3) Criar qualquer código em JS ou TS
+    (pode ser até um console.log)
+    4) Utilizem os comandos do git:
+        - git status (verifica alterações)
+        - git branch (lista e cria novas branches)
+        - git checkout (entra em uma branch)
+        - git add (prepara as alterações para envio)
+        - git commit (marca uma nova versão com uma mensagem - IMPORTANTÍSSIMO)
+        - git push origin nome-da-branch (envia as alterações para o Github)
+
+    5) Verifique se o arquivo criado foi enviado para uma nova branch. ATENÇÃO: não dê push diretamente no main. 
+
+*/
+
 function somar(a, b) {
   return a + b;
 }
